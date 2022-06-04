61491111f766cf7f49f258368a86ecfbbec910ca
GuilhermeDelfino <guilherme.narciso@sptech.school>
2022-06-04 11:05:34 -0300

    :tada: Header, Home e Footer finalizado

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/app/public"));
-app.use(express.static(__dirname + "/app/public/html"));
+app.use(express.static(__dirname + "/app/public/view"));
 
 app.get("/", (req, res) => {
   res.render("index");
